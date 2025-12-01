82a03eeb3844455d71398839eb387ef729f7e660
Subhlaravel108 <laravel.developer.108@gmail.com>
2025-12-01 10:11:47 +0530

    update

--- utils/email.js
+++ utils/email.js
@@ -13,19 +13,21 @@ const createTransporter = () => {
       port: process.env.EMAIL_PORT,
       user: process.env.EMAIL_USER
     });
+
+
     
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: parseInt(process.env.EMAIL_PORT) || 465,
+      port: parseInt(process.env.EMAIL_PORT) || 587,
       // secure: parseInt(process.env.EMAIL_PORT) === 465, // true for 465, false for other ports
-      secure:true,
+      secure:port===465,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_APP_PASSWORD
       },
-      tls: {
-        rejectUnauthorized: false // Accept self-signed certificates
-      }
+      // tls: {
+      //   rejectUnauthorized: false // Accept self-signed certificates
+      // }
     });
   } else {
     // Default to Gmail
